217cfe82646cec2f22e0ea2f6c6f731e4be47b59
Ahteshaam2 <136442236+Ahteshaam2@users.noreply.github.com>
2023-06-22 15:09:07 +0530

    database

--- src/main/java/com/example/demo4/CredentialRepository.java
+++ src/main/java/com/example/demo4/CredentialRepository.java
@@ -3,5 +3,5 @@ package com.example.demo4;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CredentialRepository extends JpaRepository<Credential, String> {
+    Credential findByUsername(String username);
 }
-//
--- src/main/java/com/example/demo4/MainController.java
+++ src/main/java/com/example/demo4/MainController.java
@@ -2,23 +2,50 @@ package com.example.demo4;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 public class MainController {
 
     @Autowired
     CredentialRepository credentialRepository;
+    @GetMapping("/")
+    public String getLandingPage(){
+        return "landingpage";
+    }
 
-    @GetMapping("/save")
-    public String saveCredential()
+   @GetMapping("/save")
+   public String saveCredential()
     {
-        Credential cd = new Credential();
-        cd.setUsername("Alex");
-        cd.setPassword("1234");
-        credentialRepository.save(cd);
+       Credential cd = new Credential();
+      cd.setUsername("Alex");
+       cd.setPassword("1234");
+       credentialRepository.save(cd);
         return "New saved";
 
+    }
+@PostMapping("/signup")
+    public String signup(@RequestParam("username") String username,@RequestParam("password") String password){
+    Credential credential = new Credential();
+    credential.setUsername("Alex");
+    credential.setPassword("1234");
+    credentialRepository.save(credential);
+    return "dashboard";
+    }
+    @PostMapping("/login")
+    public String signin(@RequestParam("username") String username, @RequestParam("password") String password) {
+
+        Credential credential = credentialRepository.findByUsername(username);
+
+        if (credential != null && credential.getPassword().equals(password)) {
+
+            return "dashboard";
+        } else {
+
+            return "redirect:/login?error";
+        }
     }
 }
